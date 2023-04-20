4d3b8bcbd310bdc67c95d32673571330fd2db39b
Saravanakumaran08011995 <saravanakumaran8195@gmail.com>
2023-04-21 00:08:23 +0530

    changes

--- routes/urlshortener.js
+++ routes/urlshortener.js
@@ -1,13 +1,12 @@
 import express from "express"
 import { urlShortenerModel } from "../models/urlShort.js"
-// import bodyParser from "body-parser"
-// app.use(bodyParser.urlencoded({ extended: true }));
 
 const router = express.Router()
 
 router.get("/all",async(req,res)=>{
     try {
         const allData = await urlShortenerModel.find({})
+        res.set('Content-Type', 'application/json');
         res.status(200).send(allData)
     } catch (error) {
         res.status(500).send({ message: "Can't fetch the details" });
@@ -38,6 +37,7 @@ router.delete("/delete/:id",async(req,res)=>{
             return res.send("There is no such a data")
         }
 
+        res.set('Content-Type', 'application/json');
         res.status(200).send("Successfully deleted")
     } catch (error) {
         res.status(500).send("Internal Server Error")
@@ -45,6 +45,7 @@ router.delete("/delete/:id",async(req,res)=>{
     }
     
 })
+
 function generateUrl() {
     let randomString = "";
     let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -52,12 +53,11 @@ function generateUrl() {
 
     for (var i = 0; i < 5; i++) {
         randomString += characters.charAt(
-            Math.floor(Math.random() * (charactersLength + i)) // just for randomly obtaining
+            Math.floor(Math.random() * (charactersLength + i))
         );
     }
     console.log(randomString)
     return randomString
 }
-
 
 export const urlRoutes = router
